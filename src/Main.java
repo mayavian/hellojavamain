15b8b9f4757d4eaf205116abcdd058fc550ab3d3
mayavian <mayavian@163.com>
2018-11-10 16:20:02 +0800

    feature 1

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
 public class Main {
 
     public static void main(String[] args) {
+
         System.out.println("Hello World 1!");
+
+        System.out.println("New Feature 1");
+        
     }
 
 }
